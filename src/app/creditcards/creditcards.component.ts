b2469a466da21f1414754c8b61b4712111507f21
SachinDevatar7 <sachindevatarhs777@gmail.com>
2024-11-18 22:07:20 -0600

    Integration-with-API

--- src/app/creditcards/creditcards.component.ts
+++ src/app/creditcards/creditcards.component.ts
@@ -1,192 +1,70 @@
 import { Component, ViewChild } from '@angular/core';
-import {CreditCard} from '../models/credit-card';
+import { CreditCard } from '../models/credit-card';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
-import {MatPaginator} from "@angular/material/paginator";
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-const TABLE_DATA: CreditCard[] = [
-  {
-    id: 1,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 2,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  },
-  {
-    id: 3,
-    name: "Bank of America",
-    description: "Bank of America offers customers with variety of options",
-    bankName: 'Bank of America',
-    maxCredit: 3000,
-    interestRate: 10,
-    active: false,
-    recommendedScore: "700-900",
-    annualFee: 4,
-    termsAndConditions: 'The following conditions are applicable for this card',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08'
-  }
-];
+import { CreditcardsService } from '../services/creditcards.service';
 
 @Component({
   selector: 'app-creditcards',
   templateUrl: './creditcards.component.html',
-  styleUrls: ['./creditcards.component.scss']
+  styleUrls: ['./creditcards.component.scss'],
 })
 export class CreditcardsComponent {
+  creditcards: CreditCard[] = [];
 
-  displayColums = ["select", "id", "name", "description",
-    "bankName", "maxCredit", "interestRate", "active", "recommendedScore" 
-  ]
+  constructor(private creditcardsService: CreditcardsService) {
+    this.creditcardsService.getCreditCards().subscribe((data: CreditCard[]) => {
+      this.creditcards = data;
+      console.log('HERE IS THE DATA', this.creditcards);
 
-  dataSource = new MatTableDataSource(TABLE_DATA);
+      this.dataSource = new MatTableDataSource(this.creditcards);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  // If the constructor fails to get the data it will come here and get null/empty data.
+  dataSource = new MatTableDataSource(this.creditcards);
+  displayColums = [
+    'select',
+    'id',
+    'name',
+    'description',
+    'bankName',
+    'maxCredit',
+    'interestRate',
+    'active',
+    'recommendedScore',
+    'actions'
+  ];
+
+  //dataSource = new MatTableDataSource(TABLE_DATA);
 
   selection = new SelectionModel(true, []);
 
-  @ViewChild(MatPaginator) paginator!:MatPaginator;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-/**
- * Lifecycle hook that is called after the component's view has been fully initialized.
- * This method assigns the paginator and sorting functionality to the dataSource for
- * the Angular Material table, enabling pagination and sorting of data.
- * The paginator and sort references are accessed via @ViewChild, which are only available
- * after the view is initialized, making ngAfterViewInit the appropriate hook for this setup.
- */
-ngAfterViewInit() {
-  this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
-}
+  /**
+   * Lifecycle hook that is called after the component's view has been fully initialized.
+   * This method assigns the paginator and sorting functionality to the dataSource for
+   * the Angular Material table, enabling pagination and sorting of data.
+   * The paginator and sort references are accessed via @ViewChild, which are only available
+   * after the view is initialized, making ngAfterViewInit the appropriate hook for this setup.
+   */
+  //ngAfterViewInit() {}
 
-/**
- * Handles the selection or deselection of a table row.
- * This method toggles the selection state of the given credit card row
- * by using the SelectionModel to track which rows are selected.
- * The 'as never' cast is used to satisfy type requirements when working with SelectionModel.
- * 
- * @param row - The CreditCard object representing the selected/deselected row.
- */
-selectHandler(row: CreditCard) {
-  this.selection.toggle(row as never);
-}
-
+  /**
+   * Handles the selection or deselection of a table row.
+   * This method toggles the selection state of the given credit card row
+   * by using the SelectionModel to track which rows are selected.
+   * The 'as never' cast is used to satisfy type requirements when working with SelectionModel.
+   *
+   * @param row - The CreditCard object representing the selected/deselected row.
+   */
+  selectHandler(row: CreditCard) {
+    this.selection.toggle(row as never);
+  }
 }
