aad6d895c05da2c8cc5a2cb5addb321766ed68c2
SachinDevatar7 <sachindevatarhs777@gmail.com>
2024-11-20 23:33:12 -0600

    delete-funcationality

--- src/app/creditcards/delete/delete.component.ts
+++ src/app/creditcards/delete/delete.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CreditcardsService } from 'src/app/services/creditcards.service';
 
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
-  styleUrls: ['./delete.component.scss']
+  styleUrls: ['./delete.component.scss'],
 })
 export class DeleteComponent {
+  creditCardId!: Number;
+  constructor(
+    private router: ActivatedRoute,
+    private creditCardService: CreditcardsService,
+    private route: Router
+  ) {
+    this.creditCardId = parseInt(this.router.snapshot.paramMap.get('id') || '');
 
+    this.creditCardService
+      .deleteCreditCard(this.creditCardId)
+      .subscribe((data) => {
+        alert('Credit Card Deleted');
+        this.route.navigate(['creditcards'])
+      });
+  }
 }
